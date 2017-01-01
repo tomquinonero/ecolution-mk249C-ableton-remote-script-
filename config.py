4da2e840d3322ad545893b680f589193356a8b1d
Tom Quinonero <tom@tomtomtom.fr>
2017-01-01 23:09:33 +0100

    init script

---
+++ config.py
@@ -0,0 +1,43 @@
+from consts import *
+TRANSPORT_CONTROLS = {'STOP': -1,
+ 'PLAY': -1,
+ 'REC': -1,
+ 'LOOP': -1,
+ 'RWD': -1,
+ 'FFWD': -1}
+DEVICE_CONTROLS = (GENERIC_ENC1,
+ GENERIC_ENC2,
+ GENERIC_ENC3,
+ GENERIC_ENC4,
+ GENERIC_ENC5,
+ GENERIC_ENC6,
+ GENERIC_ENC7,
+ GENERIC_ENC8)
+VOLUME_CONTROLS = ((-1, -1),
+ (-1, -1),
+ (-1, -1),
+ (-1, -1),
+ (-1, -1),
+ (-1, -1),
+ (-1, -1),
+ (-1, -1))
+TRACKARM_CONTROLS = (-1, -1, -1, -1, -1, -1, -1, -1)
+BANK_CONTROLS = {'TOGGLELOCK': -1,
+ 'BANKDIAL': -1,
+ 'NEXTBANK': -1,
+ 'PREVBANK': -1,
+ 'BANK1': -1,
+ 'BANK2': -1,
+ 'BANK3': -1,
+ 'BANK4': -1,
+ 'BANK5': -1,
+ 'BANK6': -1,
+ 'BANK7': -1,
+ 'BANK8': -1}
+CONTROLLER_DESCRIPTION = {'INPUTPORT': 'Evolution MK-249C',
+ 'OUTPUTPORT': 'Evolution MK-249C',
+ 'CHANNEL': -1}
+MIXER_OPTIONS = {'NUMSENDS': 2,
+ 'SEND1': (-1, -1, -1, -1, -1, -1, -1, -1),
+ 'SEND2': (-1, -1, -1, -1, -1, -1, -1, -1),
+ 'MASTERVOLUME': -1}
---
+++ consts.py
@@ -0,0 +1,20 @@
+""" The following consts should be substituted with the Sys Ex messages for requesting
+a controller's ID response and that response to allow for automatic lookup"""
+ID_REQUEST = 0
+ID_RESP = 0
+GENERIC_ENC1 = 10
+GENERIC_ENC2 = 11
+GENERIC_ENC3 = 12
+GENERIC_ENC4 = 13
+GENERIC_ENC5 = 91
+GENERIC_ENC6 = 93
+GENERIC_ENC7 = 94
+GENERIC_ENC8 = 95
+GENERIC_ENCODERS = (GENERIC_ENC1,
+ GENERIC_ENC2,
+ GENERIC_ENC3,
+ GENERIC_ENC4,
+ GENERIC_ENC5,
+ GENERIC_ENC6,
+ GENERIC_ENC7,
+ GENERIC_ENC8)
